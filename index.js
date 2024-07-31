f6248823dd86a69ad89840e6e277751d5dc20821
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-07-30 20:49:23 -0500

    Day 1 Baladium's Drive in Console Logs

---
+++ index.js
@@ -0,0 +1,30 @@
+// Baladium's Drive with Lyrics by Alex "Man On The Internet" Beckham
+// Verse 1
+console.log("Once you choose your path, you never go back")
+console.log("Fools run, but I will choose to attack")
+console.log("The harshest winds waiting for the storm")
+console.log("Free us from the chance to mourn!"); 
+
+// Chorus
+function chorus() {
+    console.log("When you look to the wind and you touch the sky");
+    console.log("When you look at a world that dreams not to fly");
+    console.log("Will the touchdown rip the tears from your eyes?");
+    console.log("Will it give you reason and drive?");
+    
+    console.log("With the wind at our back, we are flying free");
+    console.log("In the sky, the one thing they won’t take from me");
+    console.log("Soaring on to the place that I’m meant to be");
+    console.log("I will fly!")
+    console.log("I will fly!")
+}
+
+chorus();
+
+// Verse 2
+console.log("When the charming ends, the cobra will strike");
+console.log("The sun falls, so launches the dragonfly!");
+console.log("The harshest winds trapped in which you swing");
+console.log("Upturn your everything!");
+
+chorus();
