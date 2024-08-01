8a29dd3f60d1d850f621df8534510046158456f4
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-07-31 21:17:31 -0500

    Day 2 Overthrone in Console Logs

---
+++ index-2.js
@@ -0,0 +1,96 @@
+// "Overthrone" by Dean "'ongestsoloever" MiZarzo
+function song() {
+    let r;
+    // Verse 1
+    console.log("Do not be afraid")
+    console.log("Your savior has arrived")
+    console.log("Rejoice, hear the word")
+    console.log("Of the angel in disguise")
+    console.log("I know what makes you human")
+    console.log("I know the things you dread")
+    console.log("I'll whisper the deception")
+    console.log("Planting thoughts inside your head")
+    console.log("The ancient pretender");
+    console.log("From whom you can't hide");
+    console.log("The hand guiding history");
+    console.log("To ruin mankind");
+    console.log("Try to escape what you");
+    console.log("Fear deep inside");
+    console.log("Run while you can");
+    console.log("Feel the pain behind your eyes");
+
+    r = refrain();
+    console.log(r);
+
+    // Verse 2
+    console.log("Why attempt to escape");
+    console.log("The terrors that you fear");
+    console.log("Why bother trying to hide");
+    console.log("From the whisper in your ear");
+    console.log("Your consciousness is fragile");
+    console.log("Your mind will surely break");
+    console.log("When facing down a demon");
+    console.log("Who'll impersonate your face");
+    console.log("Can't you see you've been");
+    console.log("Bowing down to me");
+    console.log("I will intercept your prayers");
+    console.log("Steal your dreams");
+    console.log("Now your god's an absentee");
+    console.log("Your foolishness will be your legacy");
+    console.log("Committed the crime of opposing me");
+    console.log("You'll pay the price with your identity");
+    console.log("Now perish with your kind");
+
+    console.log(r);
+
+    // Breakdown
+    console.log("Do you know my face");
+    console.log("It's been some time between");
+    console.log("Can you recall the");
+    console.log("Terror on your screen");
+    console.log("You came to know me first with");
+    console.log("Twisted flesh and broken bone");
+    console.log("You've been frightened by the monsters but");
+    console.log("Your skin is not your own");
+    console.log("Now stare into the mirror");
+    console.log("And behold your wretched face");
+    console.log("Do you understand that you are not");
+    console.log("The child you replaced");
+    console.log("You'll give in to the hunger");
+    console.log("Go and make yourself some friends");
+    console.log("The might of the subconscious");
+    console.log("Binding us until the end");
+    console.log("A perfect simulacrum");
+    console.log("Bred to mimic, born to kill");
+    console.log("Now go and join the others");
+    console.log("And do thy creator's will");
+
+    console.log(r);
+
+    console.log("Rejoice");
+    console.log("Rejoice");
+    console.log("Rejoice");
+}
+
+function refrain() {
+    let words = ""
+    words += "A new tradition \n"
+    words += "Leading the world by the hand \n"
+    words += "Forsaking the shepherd \n"
+    words += "Follow my word till the end \n"
+    words += "Your god imprisoned \n"
+    words += "Forgotten and buried in sand \n"
+    words += "Do not be afraid \n"
+    words += "Rejoice \n"
+    words += "Because your world's been overthrown \n"
+    words += "Retreat to your homes and die alone \n"
+    words += "Fate unfortunate \n"
+    words += "Your life's been replaced with an alternate \n"
+    words += "Ancient sabotage \n"
+    words += "Concealed by angelic camouflage \n"
+    words += "Conquered easily \n"
+    words += "Bow down to a synthetic deity \n"
+    return words
+}
+
+song();
